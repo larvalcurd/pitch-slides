9370c9f1c430813999fae3f0fcba062315bb9d3d
larvalcurd <110689984+larvalcurd@users.noreply.github.com>
2025-12-07 12:49:18 +0300

    Update PresentationTitle.tsx

--- src/components/PresentationTitle/PresentationTitle.tsx
+++ src/components/PresentationTitle/PresentationTitle.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import styles from './PresentationTitle.module.css';
 
 type PresentationTitleProps = {
     title: string;
     onTitleChange: (newTitle: string) => void;
 };
 
-export default function PresentationTitle({ title, onTitleChange }: PresentationTitleProps) {
+export function PresentationTitle({ title, onTitleChange }: PresentationTitleProps) {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newTitle = e.target.value;
         onTitleChange(newTitle);
-        console.log(newTitle);
+        console.log(newTitle);  
     };
 
     return (
-        <div className={styles.container}>
-            <label className={styles.label}>Presentation Title</label>
             <input
-                className={styles.input}
                 value={title}
                 onChange={handleChange}
                 placeholder="Enter Presentation title"
             />
-        </div>
     );
 }

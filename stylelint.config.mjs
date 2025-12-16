46453382308e52057ae4eea83c93f7d6bc12c7f5
larvalcurd <110689984+larvalcurd@users.noreply.github.com>
2025-12-16 14:50:53 +0300

    Update stylelint.config.mjs

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-prettier/recommended"
-  ],
-  plugins: ["stylelint-prettier"],
-  ignoreFiles: ["node_modules/**", "dist/**"]
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-prettier/recommended"
+    ],
+    plugins: ["stylelint-prettier"],
+    overrides: [
+        {
+            files: ['**/*.{js,jsx,ts,tsx}'],
+            customSyntax: 'postcss-styled-syntax'
+        },
+        {
+            files: ['**/*.{css,scss,html}'],
+        }
+    ],
+    ignoreFiles: ["node_modules/**", "dist/**"]
 };

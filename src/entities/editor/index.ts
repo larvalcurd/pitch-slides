2464587931061d042f8ae693395a64d28123ca90
larvalcurd <110689984+larvalcurd@users.noreply.github.com>
2025-12-30 17:18:08 +0300

    Update index.ts

--- src/entities/editor/index.ts
+++ src/entities/editor/index.ts
@@ -1,7 +1,7 @@
 export * from './types/EditorTypes';
 export * from './types/UIStateTypes';
 
-export * from './utils/EditorUtils';
+export * from './utils/editorUtils';
 export * from './utils/dragUtils';
 export * from './utils/resizeUtils';
 
